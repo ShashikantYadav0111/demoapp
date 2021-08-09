f804b74c0ae864c6d41441aba3a839acdc2893ac
Shashikant <yadavshashikant43@gmail.com>
2021-08-09 15:04:20 +0530

    final edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import './index.css'
 
-function App() {
+const names = [];
+
+const Function = () => {
+
+  const [newplayer, setNewPlayer] = useState('');
+  const [players, setPlayer] = useState(names);
+
+  function setChange(e) {
+    setNewPlayer(e.target.value);
+  }
+
+  function addPlayer() {
+    const newList = players.concat({ id: uuidv4(), name: newplayer });
+    setPlayer(newList);
+    setNewPlayer('');
+  }
+
+  function delPlayer(id) {
+    const newlist = players.filter(item => item.id !== id);
+    setPlayer(newlist);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello this is demo page</h1>
+    <div className='outerbox'>
+      <div className='header'>
+        <h1>
+          To-Do List
+        </h1>
+      </div>
+      <div className='innerbox'>
+        <div className='inputBox'>
+          <input value={newplayer} type='text' onChange={setChange} placeholder='Enter your task' />
+          <button  onClick={addPlayer}>+</button>
+        </div>
+        <div className='listItems'>
+          <ul>
+            {players.map((item) => {
+              return (
+              <li  key={item.id}>
+                <button  onClick={() => delPlayer(item.id)}>X</button>
+                {item.name}
+              </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
+export default Function;
